23710a1af8619afe2c65dc53427c496c328d4a22
Giovanni.iacono98 <98giovanniiacono98@gmail.com>
2024-09-05 17:10:11 +0200

    aggiunta di alcuni test

--- src/test/java/Project/S14_L2/S14L2ApplicationTests.java
+++ src/test/java/Project/S14_L2/S14L2ApplicationTests.java
@@ -1,13 +1,35 @@
 package Project.S14_L2;
 
+import Project.S14_L2.Entities.Drinks;
+import Project.S14_L2.Entities.Menu;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+
+import static org.junit.jupiter.api.Assertions.assertSame;
+import static org.springframework.test.util.AssertionErrors.assertEquals;
+
 
 @SpringBootTest
 class S14L2ApplicationTests {
 
 	@Test
-	void contextLoads() {
+	 void calcolaPrezzoDrink() {
+		AnnotationConfigApplicationContext ctx = new AnnotationConfigApplicationContext(S14L2Application.class);
+		Menu m1= (Menu) ctx.getBean("primo_menu");
+
+		assertEquals("siOno",70.0,m1.getPrezzoDrink());
+	}
+@Test
+@Disabled
+	void sameMemoryLocationDrink(){
+		AnnotationConfigApplicationContext ctx = new AnnotationConfigApplicationContext(S14L2Application.class);
+		Drinks d1= (Drinks) ctx.getBean("coca");
+		Drinks d2= (Drinks) ctx.getBean("mojito");
+		assertSame(d1,d2);
+
 	}
 
 }
